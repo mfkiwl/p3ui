7aed6c3cca99636be3c9f347b952756e40e0ef4a
Martin Rudoff <martin.rudoff@tomra.com>
2021-07-24 14:02:45 +0200

    tidy: default to nearest

--- cpp/p3/source/p3/backend/OpenGL2RenderBackend.cpp
+++ cpp/p3/source/p3/backend/OpenGL2RenderBackend.cpp
@@ -27,8 +27,8 @@ namespace p3
         TextureId id;
         glGenTextures(1, &reinterpret_cast<GLuint&>(id));
         glBindTexture(GL_TEXTURE_2D, reinterpret_cast<GLuint&>(id));
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
