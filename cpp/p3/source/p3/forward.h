b3d85db4234cfab5f6f5cd6a27633fe65ee767e7
Martin Rudoff <martin.rudoff@tomra.com>
2021-08-28 03:39:35 +0200

    create fwd header

---
+++ cpp/p3/source/p3/forward.h
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace p3
+{
+    class Button;
+    class CheckBox;
+    class ChildWindow;
+    class Collapsible;
+    class Color;
+    class ColorEdit;
+    class ComboBox;
+    class Layout;
+    class Image;
+    template<typename T> class InputScalar;
+    class InputText;
+    class Menu;
+    class MenuItem;
+    class MenuBar;
+    class Node;
+    class Plot;
+    class Popup;
+    class ProgressBar;
+    class ScrollArea;
+    template<typename T> class Slider;
+    class StyleBlock;
+    class Tab;
+    class Table;
+    class Text;
+    class Texture;
+    class UserInterface;
+    class Theme;
+    class Window;
+}
--- cpp/p3_python/source/p3/python/p3ui.h
+++ cpp/p3_python/source/p3/python/p3ui.h
@@ -30,44 +30,12 @@
 #include <pybind11/functional.h>
 
 #include <p3/log.h>
+#include <p3/forward.h>
 namespace py = pybind11;
 
 #include "Promise.h"
 
 
-//
-// NOTE: use fwd header?
-namespace p3
-{
-    class Button;
-    class CheckBox;
-    class ChildWindow;
-    class Collapsible;
-    class Color;
-    class ColorEdit;
-    class ComboBox;
-    class Layout;
-    class Image;
-    template<typename T> class InputScalar;
-    class InputText;
-    class Menu;
-    class MenuItem;
-    class MenuBar;
-    class Node;
-    class Plot;
-    class Popup;
-    class ProgressBar;
-    class ScrollArea;
-    template<typename T> class Slider;
-    class StyleBlock;
-    class Tab;
-    class Table;
-    class Text;
-    class Texture;
-    class UserInterface;
-    class Theme;
-    class Window;
-}
 
 namespace p3::python
 {
