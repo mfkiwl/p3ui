30da8fc29f5ff42650b1bfe1693ae19c5f3ed53f
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-01 05:36:00 +0100

    fix clear color

--- cpp/p3_python/source/p3/python/Surface.cpp
+++ cpp/p3_python/source/p3/python/Surface.cpp
@@ -291,7 +291,7 @@ namespace p3::python
             p1.y += window->WindowPadding.y - window->WindowBorderSize - item_spacing.y; // really?
             ImVec2 p2{ p1.x + float(width), p1.y + float(height) };
 
-            glClearColor(0.f, 0.f, 0.5f, 0.2f);
+            glClearColor(0.f, 0.f, 0.0f, 0.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             auto canvas = _skia_surface.value().attr("getCanvas")();
             canvas.attr("save")();
