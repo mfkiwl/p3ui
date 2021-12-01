a65b81da8c861ba55adfb834e9e7d9ce42ac5d66
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-01 16:34:09 +0100

    fix: clamp to edge

--- cpp/p3/source/p3/backend/OpenGLRenderTarget.cpp
+++ cpp/p3/source/p3/backend/OpenGLRenderTarget.cpp
@@ -46,8 +46,10 @@ namespace p3
             _width, _height,
             0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, 
             reinterpret_cast<GLuint const&>(_texture_id), 0);
 
