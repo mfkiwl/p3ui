24370706711f4e8cb245eb08139e9d1f2ca160dc
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-02 21:40:31 +0100

    impl: plot sinus to figure

--- python/plot/main.py
+++ python/plot/main.py
@@ -1,16 +1,21 @@
 from p3ui import *
+from p3.matplotlib.matplotlib_figure import MatplotlibFigure
 import asyncio
-
-import matplotlib
-
-matplotlib.use("module://p3.matplotlib.backend")
+import numpy as np
 
 
 async def main():
     window = Window(title='Canvas Demo')
     window.position = (50, 50)
     window.size = (800, 640)
-    await window.serve(UserInterface(content=None))
+
+    figure = MatplotlibFigure()
+
+    with figure as plt:
+        x = np.arange(0., 10., 0.2)
+        plt.plot(x, np.sin(x))
+
+    await window.serve(UserInterface(content=figure))
 
 
 asyncio.run(main())
