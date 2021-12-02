4bab3cb40abfe0e9e6c9cc8e984250b1eae60486
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-02 21:15:55 +0100

    impl: plot demo window

--- python/plot/main.py
+++ python/plot/main.py
@@ -1,3 +1,16 @@
+from p3ui import *
+import asyncio
+
 import matplotlib
 
 matplotlib.use("module://p3.matplotlib.backend")
+
+
+async def main():
+    window = Window(title='Canvas Demo')
+    window.position = (50, 50)
+    window.size = (800, 640)
+    await window.serve(UserInterface(content=None))
+
+
+asyncio.run(main())
