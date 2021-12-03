81f53b658fed17790982dea1c1a4da19fa13c6ac
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-03 04:17:47 +0100

    chore: cleanup main

--- python/plot/main.py
+++ python/plot/main.py
@@ -1,43 +1,26 @@
-import matplotlib.pyplot as plt
 from p3ui import *
-from p3.matplotlib.matplotlib_figure import MatplotlibFigure
 import asyncio
 import numpy as np
+from bar_plot import BarPlot
+from filled_plot import FilledPlot
 
-
-def sin_generator():
-    shift = 0.0
-    while True:
-        shift += 0.1
-        x = np.arange(0 + shift, 10.0 + shift, 0.1)
-        yield x, np.sin(x)
-
-
-async def update(figure):
-    generator = sin_generator()
-    x, y = next(generator)
-    print(len(x))
-    while True:
-        x, y = next(generator)
-        with figure as ax:
-            ax.clear()
-            ax.plot(x, y)
-        await asyncio.sleep(0.01)
 
 async def main():
-    window = Window(title='Canvas Demo')
+    window = Window(title='matplotlib')
     window.position = (50, 50)
     window.size = (800, 640)
 
-    figure = MatplotlibFigure()
+    dpi = window.monitor.dpi
 
-    asyncio.create_task(update(figure))
-
-    with figure as plt:
-        x = np.arange(0., 10., 0.2)
-        plt.plot(x, np.sin(x))
-
-    await window.serve(UserInterface(content=figure))
+    await window.serve(UserInterface(
+        content=Column(
+            children=[
+                Button(label='button 1'),
+                Row(children=[BarPlot(dpi), FilledPlot(dpi)]),
+                Button(label='button 2'),
+                Row(children=[FilledPlot(dpi), BarPlot(dpi)])
+            ]
+        )))
 
 
 asyncio.run(main())
