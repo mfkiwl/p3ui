0cf4655be6600e112bff284c05b0b2385633f12d
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-04 14:22:02 +0100

    align with plot demo

--- demos/gallery/main.py
+++ demos/gallery/main.py
@@ -6,7 +6,7 @@ from gallery import Gallery
 async def main():
     window = Window(title='gallery')
     window.position = (50, 50)
-    window.size = (1024, 768)
+    window.size = (1000, 900)
     gallery = Gallery(window)
     await window.serve(gallery)
     gallery.shutdown()
