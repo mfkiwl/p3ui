2e712f286c17edbc3ee89f818cc4aa7060ab9255
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-04 12:48:27 +0100

    load font

--- demos/matplotlib/main.py
+++ demos/matplotlib/main.py
@@ -1,15 +1,16 @@
 from p3ui import *
 import asyncio
 import matplotlib
+import pathlib
 from bar_chart import BarChart
 from filled_chart import FilledChart
 from gradient_chart import GradientChart
 from line_styles import LineStyles
 
-imgui_font_size = 13
+font_size_px = 13
 #
 # matplotlib expects points. we use default dpi and compute 13px in points..
-matplotlib.rcParams.update({'font.size': pixels_to_points(imgui_font_size)})
+matplotlib.rcParams.update({'font.size': pixels_to_points(font_size_px)})
 
 
 async def main():
@@ -17,17 +18,24 @@ async def main():
     window.position = (50, 50)
     window.size = (800, 800)
 
-    await window.serve(UserInterface(content=ScrollArea(content=Column(
-        width=(100|em, 0, 0), height=(90 | em, 0, 0),
+    user_interface = UserInterface(content=ScrollArea(content=Column(
+        width=(100 | em, 0, 0), height=(50 | em, 0, 0),
         children=[
-            Text('ImGui-Text @ 13px'),
+            Text(f'ImGui-Text @ {font_size_px}px'),
             Row(padding=(0 | px, 0 | px),
                 height=(1 | px, 1, 1),
-                children=[BarChart(), FilledChart()]),
-            LineStyles(),
-            GradientChart()
+                children=[BarChart(), FilledChart(), GradientChart()]),
+            LineStyles(
+                height=(1 | px, 1.3, 1)
+            )
         ]
-    ))))
+    )))
+
+    assets = pathlib.Path(__file__).parents[1].joinpath('assets').absolute()
+    user_interface.load_font(assets.joinpath("DroidSans.ttf").as_posix(), font_size_px)
+    user_interface.merge_font(assets.joinpath("MaterialIcons-Regular.ttf").as_posix(), font_size_px)
+
+    await window.serve(user_interface)
 
 
 asyncio.run(main())
