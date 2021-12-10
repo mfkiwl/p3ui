a8f4a70b4cb3f28092186b49be1a7f147e60dc33
Martin Rudoff <martin.rudoff@tomra.com>
2021-12-10 18:36:46 +0100

    refac: use widgets

--- demos/icons/main.py
+++ demos/icons/main.py
@@ -3,16 +3,44 @@ import asyncio
 from p3ui import *
 
 
+def sliced(seq, slice_length):
+    l = list(seq)
+    for i in range(0, len(l), slice_length):
+        yield l[i:i + slice_length]
+
+
 def make_paint_from_color(color):
     paint = skia.Paint()
     paint.setStrokeWidth(1.0)
     paint.setColor(color)
     return paint
 
+
 font_size = 24
 typeface = skia.Typeface('Consolas')
 font = skia.Font(typeface, font_size, 1.0, 0.0)
 paint = skia.Paint(AntiAlias=True, Color=skia.ColorBLACK)
+
+
+class Icon(Surface):
+
+    def __init__(self, dom, base_size=24, pixel_size=32, *, color=Color('yellow')):
+        self.color = skia.Color(*color.rgba)
+        backbuffer = skia.Surface(pixel_size, pixel_size)
+        super().__init__(
+            width=(pixel_size | px, 0, 0),
+            height=(pixel_size | px, 0, 0)
+        )
+        with backbuffer as canvas:
+            canvas.scale(pixel_size / base_size, pixel_size / base_size)
+            dom.setContainerSize(skia.Size(pixel_size, pixel_size))
+            dom.render(canvas)
+        image = backbuffer.makeImageSnapshot()
+        with self as canvas:
+            # filter = skia.ColorFilters.Blend(0x00FFFFFF, skia.BlendMode.kDstIn)
+            filter = skia.ColorMatrixFilter.MakeLightingFilter(skia.ColorWHITE, self.color)
+            paint = skia.Paint(ColorFilter=filter)
+            canvas.drawImage(image, 0, 0, paint)
 
 
 class IconBrowser(Column):
@@ -28,42 +56,26 @@ class IconBrowser(Column):
             selected_index=0,
             on_change=self._update
         )
-        self._surface = Surface()
+        self._scroll_area = ScrollArea()
         super().__init__(children=[
             self._style_combo,
-            ScrollArea(content=self._surface)
+            self._scroll_area
         ], **kwargs)
         self._update(0)
 
     def _update(self, index):
+        self._content_column = Column()
+        self._scroll_area.content = self._content_column
         style = self._style_combo.options[index]
-        print(style)
         categories = self._provider.categories(style)
-        with self._surface as canvas:
-            canvas.clear(skia.ColorWHITE)
-            x, y = 20, 20 + 16
-            for category in categories:
-                canvas.drawString(category, x, y, font, paint)
-                x += 50
-                y += font_size
-                icons = self._provider.icons(style, category)
-                for icon in icons:
-                    dom = self._provider.load_icon(style, category, icon)
-                    canvas.save()
-                    canvas.translate(x, y + self.spacing)
-                    canvas.scale(self.icon_size / 24, self.icon_size / 24)
-                    dom.setContainerSize(skia.Size(self.icon_size, self.icon_size))
-                    dom.render(canvas)
-                    x += self.icon_size + self.spacing * 2
-                    canvas.restore()
-                    if x > 1100:
-                        x = 70
-                        y += self.icon_size + self.spacing * 2
-                y += self.icon_size * 3
-                x = 20
-
-        self._surface.style.width = (1200 | px, 0, 0)
-        self._surface.style.height = (y | px, 0, 0)
+        for category in categories:
+            self._content_column.add(Row(children=[Text(category)]))
+            icons = self._provider.icons(style, category)
+            for slice in sliced(icons, 20):
+                doms = [self._provider.load_icon(style, category, name) for name in slice]
+                self._content_column.add(Row(padding=(0 | px, 0 | px), children=[
+                    Icon(dom, 24, 32) for dom in doms
+                ]))
 
 
 async def main():
